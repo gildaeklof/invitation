0d55acdbbadb147b7ef1724a305f39f8947ec9f5
Gilda Eklof <gilda@eklof.se>
2021-03-01 12:33:16 +0100

    added thank you after registration

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,8 @@
 const goDown = document.querySelector(".redirect");
 const goUp = document.querySelector(".arrowgr");
 const arrow = document.querySelector(".arrow");
+const regButton = document.querySelector(".register");
+const signUp = document.querySelector(".signup-info");
 
 goDown.addEventListener("click", () => {
   window.location.href = "#signup";
@@ -8,6 +10,12 @@ goDown.addEventListener("click", () => {
 
 goUp.addEventListener("click", () => {
   window.location.href = "#main";
+});
+
+regButton.addEventListener("click", () => {
+  signUp.innerHTML =
+    "Tack för din anmälan!<br><br> Din anmälan är mottagen och du kommer få ett mejl med mer info inom kort.";
+  regButton.innerHTML = "Tack!";
 });
 
 /*function showArrow() {
